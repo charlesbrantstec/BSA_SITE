8bc5b270979458a8fc11e32696662ee6dcf21d6b
charles <charlesbrantstec@gmail.com>
2021-01-11 13:09:59 -0600

    attempting to resolve stylesheet linking issue

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 //@RestController
@@ -10,6 +9,11 @@ public class HomeController {
     @GetMapping("/home")
     public String homePage(){
         return "home";
+    }
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
     }
 
 }
